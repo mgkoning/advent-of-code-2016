5581b168bb1b23f6fa88c1373d2d67f1a8d49fb4
Michiel Koning <m.g.koning@gmail.com>
2016-12-20 08:27:35 +0100

    day 19, part 2 (horribly inefficient)

--- day19-an-elephant-named-joseph/program.go
+++ day19-an-elephant-named-joseph/program.go
@@ -8,6 +8,8 @@ var puzzleInput = 3014603
 func main() {
 	determineAnswer(exampleInput)
 	determineAnswer(puzzleInput)
+	determineAnswerPart2(exampleInput)
+	determineAnswerPart2(puzzleInput)
 }
 
 func determineAnswer(elvesCount int) {
@@ -38,4 +40,50 @@ func scanToNext(elves []bool, current int) int {
 			return next
 		}
 	}
+}
+
+type elf struct {
+	num  int
+	prev *elf
+	next *elf
+}
+
+func determineAnswerPart2(elvesCount int) {
+	var startElf = &elf{num: 1}
+	var previousElf = startElf
+	for i := 2; i <= elvesCount; i++ {
+		var newElf = elf{num: i, prev: previousElf}
+		previousElf.next = &newElf
+		previousElf = &newElf
+	}
+	startElf.prev = previousElf
+	startElf.prev.next = startElf
+	remaining := elvesCount
+	currentElf := startElf
+	for 1 < remaining {
+		eliminee := scan(currentElf, remaining/2)
+		eliminate(eliminee)
+		remaining--
+		currentElf = currentElf.next
+		if (elvesCount-remaining)%500 == 0 {
+			fmt.Println("Remaining:", remaining)
+		}
+	}
+	fmt.Println("Elf", currentElf.num, "stole all the presents.")
+}
+
+func scan(from *elf, num int) *elf {
+	result := from
+	for 0 < num {
+		result = result.next
+		num--
+	}
+	return result
+}
+
+func eliminate(eliminee *elf) {
+	next := eliminee.next
+	prev := eliminee.prev
+	next.prev = prev
+	prev.next = next
 }
