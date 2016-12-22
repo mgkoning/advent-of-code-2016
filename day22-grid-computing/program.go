28811fb34298a969b749d21b0ccbbcedb2e8a2e9
Michiel Koning <m.g.koning@gmail.com>
2016-12-22 13:40:00 +0100

    day 22, part 2: on paper much more efficient!

--- day22-grid-computing/program.go
+++ day22-grid-computing/program.go
@@ -32,6 +32,23 @@ func main() {
 		}
 	}
 	fmt.Println("Total number of viable pairs:", sum)
+	fmt.Println("Part 2")
+	nodeMap := make(map[coordinate]node)
+	maxX, maxY := 0, 0
+	for _, node := range nodes {
+		nodeMap[node.location] = node
+		maxX = max(maxX, node.location.x)
+		maxY = max(maxY, node.location.y)
+	}
+	printGrid(nodeMap, maxX, maxY)
+	// proceed to analysis on paper after dumping grid.
+}
+
+func max(one, other int) int {
+	if one < other {
+		return other
+	}
+	return one
 }
 
 type coordinate struct {
@@ -45,6 +62,16 @@ type node struct {
 	used           int
 	available      int
 	usedPercentage int
+}
+
+func printGrid(nodeMap map[coordinate]node, maxX, maxY int) {
+	for y := 0; y <= maxY; y++ {
+		for x := 0; x <= maxX; x++ {
+			node := nodeMap[coordinate{x, y}]
+			fmt.Printf("%03d/%03d ", node.used, node.size)
+		}
+		fmt.Println()
+	}
 }
 
 func determineViablePairs(nodeChannel chan node, nodes []node, sumChannel chan int) {
@@ -75,22 +102,24 @@ func determineViablePairsForNode(node node, nodes []node) int {
 
 var dfLineRegexp = regexp.MustCompile(`/dev/grid/node-x(\d+)-y(\d+)\s+(\d+)T\s+(\d+)T\s+(\d+)T\s+(\d+)%`)
 
-func parseNodes(nodeLines []string) []node {
-	result := make([]node, len(nodeLines))
+func parseNodes(nodeLines []string) (result []node) {
+	result = make([]node, len(nodeLines))
 	for i, line := range nodeLines {
 		matches := dfLineRegexp.FindStringSubmatch(line)
 		if matches == nil {
 			panic(fmt.Sprintf("Line %v not understood", line))
 		}
+		x := mustParseInt(matches[1])
+		y := mustParseInt(matches[2])
 		result[i] = node{
-			coordinate{mustParseInt(matches[1]), mustParseInt(matches[2])},
+			coordinate{x, y},
 			mustParseInt(matches[3]),
 			mustParseInt(matches[4]),
 			mustParseInt(matches[5]),
 			mustParseInt(matches[6]),
 		}
 	}
-	return result
+	return
 }
 
 func mustParseInt(s string) int {
